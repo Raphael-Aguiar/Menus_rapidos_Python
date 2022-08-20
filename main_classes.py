7b22ea0401336716f3d8843b70f9195abe6d9227
Raphael-Aguiar <raphael.aguiar@gmail.com>
2022-08-20 17:33:08 -0300

    Create main_classes.py

---
+++ main_classes.py
@@ -0,0 +1,58 @@
+
+class Master:
+
+    def __init__(self, intro, var0, var1, var2, var3, var4, var5, var6, var7,
+                 var8, var9):
+        self.intro = intro
+        self.var0 = var0
+        self.var1 = var1
+        self.var2 = var2
+        self.var3 = var3
+        self.var4 = var4
+        self.var5 = var5
+        self.var6 = var6
+        self.var7 = var7
+        self.var8 = var8
+        self.var9 = var9
+        pass
+
+    def trigger_site(self):
+
+        pass
+
+    def trigger_app(self):
+
+        pass
+
+
+class MainApps:
+    intro = 'principais aplicativos usados'
+    whatsapp = ['Whatsapp', 'https://web.whatsapp.com/']
+    todoist = ['Todoist', 'https://beta.todoist.com/app/project/1249942567']
+    memed = ['Memed', 'https://memed.com.br/plataforma/#']
+    raindrop = ['Raindrop', 'https://app.raindrop.io/my/18751286']
+    pastemagic = ['Pastemagic', 'https://pastemagic.com/']
+
+    pass
+
+
+class Google:
+    intro = 'serviços google'
+    youtube = ['Youtube', 'https://youtube.com']
+    calendar = ['Calendar', 'https://calendar.google.com/calendar']
+    gmail = ['Gmail', 'https://gmail.com']
+    google = ['Google', 'https://google.com']
+    books = ['Books', 'https://books.google.com.br/']
+    scholar = ['Scholar', 'https://scholar.google.com.br']
+    translator = ['Translator', 'https://translate.google.com.br/?hl=pt-BR']
+    keep = ['Keep', 'https://keep.google.com/u/0/#home']
+    maps = ['Maps', 'https://www.google.com/maps']
+    shopping = ['Shopping', 'https://shopping.google.com/']
+    podcasts = ['Podcasts', 'https://podcasts.google.com']
+    forms = ['Forms', 'https://docs.google.com/forms/u/0/']
+    drive = ['Drive', 'https://drive.google.com/drive/u/0/my-drive']
+    travel = ['Travel', 'https://www.google.com/travel/']
+    finances = ['Finanças', 'https://www.google.com/finance/']
+    images = ['Iamgens', 'https://www.google.com/imghp?hl=pt-brgoogle.com']
+
+    pass
